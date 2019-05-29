91aea8ce33419d7e542a37a1a9c2c5c8e79d4968
5108 <1430349989@qq.com>
2019-05-29 23:21:38 +0800

    5-29

--- python/python知识点/tkinter/Combobox下拉控件.py
+++ python/python知识点/tkinter/Combobox下拉控件.py
@@ -12,12 +12,22 @@ win.geometry('400x400+200+20')
 '''
 Combobox:下拉控件
 '''
+#绑定变量
+cv = tkinter.StringVar()
 
 
-com = ttk.Combobox(win)
+com = ttk.Combobox(win, textvariable=cv)
 
-
-
+com.pack()
+#设置下拉属性
+com['value'] = ('黑龙江','吉林','辽宁')
+#设置默认值
+com.current(0)
+def func(event):
+    print(com.get())
+    print(cv.get())
+    print('sunk is a good man!')
+com.bind('<<ComboboxSelected>>', func)
 
 
 
